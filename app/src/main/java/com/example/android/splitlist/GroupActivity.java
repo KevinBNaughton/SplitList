141e5dd3c9b5eca4e2f6d1dfcf30c3b505778841
Kevin Naughton <kevinbnaughton@gmail.com>
2019-10-26 17:23:37 -0400

    Added buttons

--- app/src/main/java/com/example/android/splitlist/GroupActivity.java
+++ app/src/main/java/com/example/android/splitlist/GroupActivity.java
@@ -3,6 +3,7 @@ package com.example.android.splitlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,9 +11,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class GroupActivity extends AppCompatActivity {
 
     private static final String TAG = "GroupActivity";
-    FirebaseAuth mFirebaseAuth;
-    FirebaseUser mFirebaseUser;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
+
+    //Create and Join Buttons
+    private Button mCreateButton;
+    private Button mJoinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
