e0e831c5da3253177413134b9aea75259796649c
Kevin Naughton <kevinbnaughton@gmail.com>
2019-10-26 17:08:59 -0400

    Added GroupActivity and Trung fixed ICON

---
+++ app/src/main/java/com/example/android/splitlist/GroupActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.splitlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class GroupActivity extends AppCompatActivity {
+
+    private static final String TAG = "GroupActivity";
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFirebaseUser;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+
+
+    }
+}
