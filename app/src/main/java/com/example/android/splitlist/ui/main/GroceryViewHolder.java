81c2394bdc9204cc4ddb041301ccd75fc0d22447
Anna Dingler <anna.dingler@bluefletch.com>
2019-10-26 14:38:11 -0400

    Updated item row

--- app/src/main/java/com/example/android/splitlist/ui/main/GroceryViewHolder.java
+++ app/src/main/java/com/example/android/splitlist/ui/main/GroceryViewHolder.java
@@ -11,15 +11,18 @@ import com.example.android.splitlist.R;
 public class GroceryViewHolder extends RecyclerView.ViewHolder {
 
     private TextView mItemName;
+    private TextView mLikes;
 
     public GroceryViewHolder(View v) {
         super(v);
 
         mItemName = v.findViewById(R.id.item_name);
+        mLikes = v.findViewById(R.id.num_upvotes);
     }
 
     public void setData(String name) {
         mItemName.setText(name);
+        mLikes.setText("1");
 
         Log.d("please set data", name);
     }
